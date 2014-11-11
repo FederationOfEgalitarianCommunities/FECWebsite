b0de58ba08392bd117bd7d79c836dd2f0f20a10c
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-11-11 02:38:58 -0500

    [#390] Add Community Admin Customizations
    Modify the CommunityAdmin to add additonal listing columns, filters and
    search fields.
    
    Add the get_community_email function to the community.admin module,
    allowing the CommunityAdmin to display a mailto link in the listing
    display's the email column.

--- fec/communities/admin.py
+++ fec/communities/admin.py
@@ -1,5 +1,6 @@
 """This module contains Admin Forms for :mod:`communities.models`."""
 from django.contrib import admin
+from django.utils.html import format_html
 from mezzanine.core.admin import DisplayableAdmin, TabularDynamicInlineAdmin
 
 from .models import Community, CommunityImage, CommunityFeed
@@ -15,6 +16,16 @@ class CommunityFeedInline(TabularDynamicInlineAdmin):
     model = CommunityFeed
 
 
+def get_community_email(obj):
+    """Return the Community's :attr:`~.models.Community.email` as a link."""
+    return format_html(
+        '<a href="mailto:{0}" target="_blank">{0}</a>'.format(obj.email))
+
+get_community_email.admin_order_field = 'email'
+get_community_email.allow_tags = True
+get_community_email.short_description = "Email"
+
+
 class CommunityAdmin(DisplayableAdmin):
     """Represents the :class:`~.models.Community` model in the admin backend.
 
@@ -24,7 +35,11 @@ class CommunityAdmin(DisplayableAdmin):
         ``Contact`` section.
 
     """
-    inlines = [CommunityFeedInline, CommunityImageInline]
+    inlines = (CommunityFeedInline, CommunityImageInline)
+    list_display = ('title', 'is_community_in_dialog', 'general_location',
+                    get_community_email, 'status', 'admin_link')
+    list_filter = ('is_community_in_dialog', 'status', 'keywords__keyword')
+    search_fields = ['title', 'general_location', 'year_founded', 'address']
     fieldsets = (
         (None, {
             "fields": [("title", "profile_image"),
