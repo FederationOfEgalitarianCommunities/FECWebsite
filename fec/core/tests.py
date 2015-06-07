2be5265680704ba9dbf98a3ee9188c6af9b75c13
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-06-07 11:25:30 -0400

    Cleanup Core Tests

--- fec/core/tests.py
+++ fec/core/tests.py
@@ -7,20 +7,20 @@ from .utils import check_pep8
 
 class Pep8Tests(TestCase):
     """Ensure the application is PEP8 compliant."""
-    def test_functional_test_pep8_compliance(self):
+    def test_functional_test_pep8(self):
         """The functional tests should be PEP8 compliant."""
         result = check_pep8([
-            'functional_tests/general_page_tests.py',
-            'functional_tests/home_page_tests.py',
             'functional_tests/admin_tests.py',
             'functional_tests/community_page_tests.py',
             'functional_tests/document_tests.py',
+            'functional_tests/general_page_tests.py',
+            'functional_tests/home_page_tests.py',
         ])
 
         self.assertEqual(result.total_errors, 0,
                          "PEP8 issues were found in the functional tests.")
 
-    def test_core_pep8_compliance(self):
+    def test_core_pep8(self):
         """The core package should be PEP8 compliant."""
         result = check_pep8([
             'core/urls.py',
@@ -37,8 +37,8 @@ class Pep8Tests(TestCase):
         result = check_pep8([
             'communities/admin.py',
             'communities/models.py',
-            'communities/views.py',
             'communities/urls.py',
+            'communities/views.py',
             'communities/templatetags/communities_tags.py',
             'communities/templatetags/communities_tags_extras.py',
             'communities/tests.py',
@@ -47,15 +47,15 @@ class Pep8Tests(TestCase):
         self.assertEqual(result.total_errors, 0,
                          "PEP8 issues were found in the communities package.")
 
-    def test_documents_pep8_compliance(self):
+    def test_documents_pep8(self):
         """The documents package should be PEP8 compliant."""
         result = check_pep8([
             'documents/admin.py',
             'documents/models.py',
-            'documents/views.py',
-            'documents/urls.py',
             'documents/templatetags/documents_tags.py',
             'documents/templatetags/documents_tags_extras.py',
+            'documents/urls.py',
+            'documents/views.py',
         ])
 
         self.assertEqual(result.total_errors, 0,
