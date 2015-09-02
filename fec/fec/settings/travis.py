d97ca28e0bb1e7c0dfd2d9cb766576b6b88f0cf6
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-09-02 14:44:07 -0400

    Update Travis SauceLabs Selenium Configuration

--- fec/fec/settings/travis.py
+++ fec/fec/settings/travis.py
@@ -2,11 +2,11 @@ import os
 
 from .test import *
 
-if 'TRAVIS' in os.environ:
-    sauce_username = os.environ['SAUCE_USERNAME']
-    sauce_access_key = os.environ['SAUCE_ACCESS_KEY']
-    SELENIUM_SERVER = "http://{}:{}@localhost:4445/wd/hub".format(
-        sauce_username, sauce_access_key)
-    SELENIUM_CAPABILITIES['tunnel-identifier'] = os.environ['TRAVIS_JOB_NUMBER']
-    SELENIUM_CAPABILITIES['build'] = os.environ["TRAVIS_BUILD_NUMBER"]
-    SELENIUM_CAPABILITIES['tags'] = [os.environ["TRAVIS_PYTHON_VERSION"], "CI"]
+
+sauce_username = os.environ['SAUCE_USERNAME']
+sauce_access_key = os.environ['SAUCE_ACCESS_KEY']
+SELENIUM_SERVER = "http://{}:{}@ondemand.saucelabs.com:80/wd/hub".format(
+    sauce_username, sauce_access_key)
+SELENIUM_CAPABILITIES['tunnelIdentifier'] = os.environ['TRAVIS_JOB_NUMBER']
+SELENIUM_CAPABILITIES['build'] = os.environ["TRAVIS_BUILD_NUMBER"]
+SELENIUM_CAPABILITIES['tags'] = [os.environ["TRAVIS_PYTHON_VERSION"], "CI"]
