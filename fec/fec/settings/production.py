5d2a94d53a04587119ade09048a18b6af577b1a2
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2015-09-03 05:42:28 -0400

    Add Production Settings & Optimized Images

---
+++ fec/fec/settings/production.py
@@ -0,0 +1,24 @@
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = [get_env_variable('ALLOWED_HOST')]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable("DB_NAME"),
+        'USER': get_env_variable("DB_USER"),
+        'PASSWORD': get_env_variable("DB_PASS"),
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    },
+}
